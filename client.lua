a2a6e8a60599135e49118c9fadb1521e73c8869d
volpeer <85888707+volpeer@users.noreply.github.com>
2023-08-15 14:51:47 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,18 @@
+Citizen.CreateThread(function()
+    RegisterFontFile('firesans') -- název soubory gfx bez koncovky gfx
+    fontId = RegisterFontId('Fire Sans') -- nazev ktery jsme dávali do in.xml
+end)
+
+Citizen.CreateThread(function()
+    RegisterFontFile('russoone') -- název soubory gfx bez koncovky gfx
+    fontId = RegisterFontId('Russo One') -- nazev ktery jsme dávali do in.xml
+end)
+
+Citizen.CreateThread(function()
+    RegisterFontFile('RobotoCondensed') -- název soubory gfx bez koncovky gfx
+    fontId = RegisterFontId('Roboto Condensed') -- nazev ktery jsme dávali do in.xml
+end)
+Citizen.CreateThread(function()
+    RegisterFontFile('Rubik') -- název soubory gfx bez koncovky gfx
+    fontId = RegisterFontId('Rubik') -- nazev ktery jsme dávali do in.xml
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'volpeer'
+description 'volpeer :D - FONT'
+version 'ALFA'
+
+client_scripts{
+    'client.lua'
+}
